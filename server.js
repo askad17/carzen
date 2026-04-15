a9647fda4cf26901cf74ab8b88495b55bf41c58f
Adelya <askad246@gmail.com>
2026-04-15 19:36:05 +0500

    не знаю что именно, но все равботает в этой версии

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+﻿const express = require('express');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const jwt = require('jsonwebtoken');
